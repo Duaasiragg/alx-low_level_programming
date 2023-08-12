ab7c6a52c5fa2f1864733338426f26c7e12d03d7
Duaasiragg <duaasiragg@gmail.com>
2023-08-11 19:53:25 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Description: 'printpossible combination numbers'
+ *Return: always 0
+ */
+int main(void)
+{
+int d, p;
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+if (d == '8' && p == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

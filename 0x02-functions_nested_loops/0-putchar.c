c9504fa4550e19ef588688e5a2c29864d84cd1a9
Duaasiragg <duaasiragg@gmail.com>
2023-08-15 15:32:25 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+int i;
+char c[] = "_putchar";
+for (i = 0; i <= 7; i++)
+{
+putchar(c[i]);
+}
+putchar('\n');
+return (0);
 }

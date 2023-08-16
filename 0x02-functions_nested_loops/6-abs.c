5918d226000b15b081d76bccbf7b6e03c828c98e
Duaasiragg <duaasiragg@gmail.com>
2023-08-15 18:31:37 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of numbers
+ *
+ * @r: the value
+ *
+ * Return: (0)
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	return (r);
+	else
+	return (r * -1);
+}

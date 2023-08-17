05518d85b8349ad416c32be03e8a21c9b146d490
Duaasiragg <duaasiragg@gmail.com>
2023-08-17 14:59:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+int c = 48;
+
+while (c <= 58)
+{
+_putchar(c);
+}
+_putchar('\n');
+}

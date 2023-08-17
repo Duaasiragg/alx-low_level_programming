277f88322a51b395b8034e971d299ac748e6b21f
Duaasiragg <duaasiragg@gmail.com>
2023-08-17 15:47:53 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - func to print lines
+ * @n: number of lines
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
